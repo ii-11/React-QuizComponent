1e1d73a517acd6ab671693434f61001a37b23979
ii-11 <ivanovski@vivaldi.net>
2020-06-17 01:35:17 +0200

    Create quiz question component

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import QuizQuestion from './QuizQuestion'
 
 let quizData = require('./quiz_data.json')
 
@@ -8,10 +9,9 @@ class Quiz extends Component {
     this.state = {quiz_position: 1}
   }
   render() {
+    const quizPozition = this.state.quiz_position - 1
     return <div>
-      <div className="QuizQuestion">
-        {quizData.quiz_questions[0].instruction_text}
-      </div>
+      <QuizQuestion quiz_question={quizData.quiz_questions[quizPozition]} />
     </div>
   }
 }
