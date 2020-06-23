820fa4e8a1f9fbffb274567d731ad114b5d365ee
ii-11 <ivanovski@vivaldi.net>
2020-06-24 01:07:42 +0200

    Add answer click handling events

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,23 +1,36 @@
-import React, { Component } from "react"
-import QuizQuestionButton from "./QuizQuestionButton"
+import React, { Component } from "react";
+import QuizQuestionButton from "./QuizQuestionButton";
 
 class QuizQuestion extends Component {
   render() {
     return (
       <main>
         <section>
-          <p>
-            {this.props.quiz_question.instruction_text}
-          </p>
+          <p>{this.props.quiz_question.instruction_text}</p>
         </section>
         <section className="buttons">
           <ul>
-            <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
+            {this.props.quiz_question.answer_options.map(
+              (answer_option, index) => {
+                return (
+                  <QuizQuestionButton
+                    clickHandler={this.handleClick.bind(this)}
+                    button_text={answer_option}
+                    key={index}
+                  />
+                );
+              }
+            )}
           </ul>
         </section>
       </main>
-    )
+    );
+  }
+  handleClick(buttonText) {
+    if (buttonText === this.props.quiz_question.answer) {
+      this.props.showNextQuestionHandler();
+    }
   }
 }
 
-export default QuizQuestion
+export default QuizQuestion;
--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,13 +1,18 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
 
 class QuizQuestionButton extends Component {
   render() {
     return (
       <li>
-        <button>{this.props.button_text}</button>
+        <button onClick={this.handleClick.bind(this)}>
+          {this.props.button_text}
+        </button>
       </li>
-    )
+    );
+  }
+  handleClick() {
+    this.props.clickHandler(this.props.button_text);
   }
 }
 
-export default QuizQuestionButton
+export default QuizQuestionButton;
